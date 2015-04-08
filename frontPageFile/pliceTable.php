ea97e3dd08c7befdce4bd26d76f787c079fdf212
kazuya <aribaba845@gmail.com>
2015-04-08 21:15:02 +0900

    priceTable 作成

--- frontPageFile/pliceTable.php
+++
@@ -1,3 +0,0 @@
-	<div class="fieldWrap">
-		<p>ここは料金表が入ります</p>
-	</div><!--/.fieldWrap -->
---
+++ frontPageFile/priceTable.php
@@ -0,0 +1,302 @@
+	<div class="fieldWrap">
+		
+		<h2><img src="" alt="">なら印刷代、デザイン料込みで格安の1190円/100枚！</h2>
+
+		<div id="priceOption">
+
+		<dl id="option">
+			<dt class="">商品仕様</dt>
+			<dd>サイズ: 一般名刺91mm×55mm</dd>
+		</dl>
+		<dl id="option">
+			<dt class="">デザイン料</dt>
+			<dd>サイズ: 一般名刺91mm×55mm</dd>
+		</dl>
+		<dl id="option">
+			<dt class="">配送料</dt>
+			<dd>サイズ: 一般名刺91mm×55mm</dd>
+		</dl>
+		<dl id="option">
+			<dt class="">オプション①</dt>
+			<dd>サイズ: 一般名刺91mm×55mm</dd>
+		</dl>
+		<dl id="option">
+			<dt class="">オプション②</dt>
+			<dd>サイズ: 一般名刺91mm×55mm</dd>
+		</dl>
+		</div>
+
+		<dl id="priceTable">
+			<dt id="priceTableDt">印刷料金</dt>
+			<dd id="priceTableDd">
+				<dl class="priceTableCell ClearFix" >
+					<dt class="busuinessCardName">超格安名刺</dt>
+					<dd>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/白黒.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/なし.png">
+							</dt>
+							<dd class="BCDesc">表クロ/裏なし</dd>
+							<span>¥000</span>
+						</dl>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/白黒.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/白黒.png">
+							</dt>
+							<dd class="BCDesc">表クロ/裏クロ</dd>
+							<span>¥000</span>
+						</dl>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/カラー.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/なし.png">
+								
+							</dt>
+							<dd class="BCDesc">表カラー/裏なし</dd>
+							<span>¥000</span>
+						</dl>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/カラー.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/白黒.png">
+								
+								</dt>
+							<dd class="BCDesc">表カラー/裏クロ</dd>
+							<span>¥000</span>
+						</dl>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/カラー.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/カラー.png">
+								
+							</dt>
+							<dd class="BCDesc">表カラー/裏カラー</dd>
+							<span>¥000</span>
+						</dl>
+
+
+
+
+					</dd>
+				</dl>
+
+				<dl class="priceTableCell ClearFix ">
+					<dt class="busuinessCardName">超格安名刺</dt>
+					<dd>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/白黒.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/なし.png">
+							</dt>
+							<dd class="BCDesc">表クロ/裏なし</dd>
+							<span>¥000</span>
+						</dl>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/白黒.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/白黒.png">
+							</dt>
+							<dd class="BCDesc">表クロ/裏クロ</dd>
+							<span>¥000</span>
+						</dl>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/カラー.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/なし.png">
+								
+							</dt>
+							<dd class="BCDesc">表カラー/裏なし</dd>
+							<span>¥000</span>
+						</dl>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/カラー.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/白黒.png">
+								
+								</dt>
+							<dd class="BCDesc">表カラー/裏クロ</dd>
+							<span>¥000</span>
+						</dl>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/カラー.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/カラー.png">
+								
+							</dt>
+							<dd class="BCDesc">表カラー/裏カラー</dd>
+							<span>¥000</span>
+						</dl>
+
+						
+
+
+					</dd>
+				</dl>
+
+
+				<dl class="priceTableCell ClearFix">
+					<dt class="busuinessCardName">超格安名刺</dt>
+					<dd>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/白黒.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/なし.png">
+							</dt>
+							<dd class="BCDesc">表クロ/裏なし</dd>
+							<span>¥000</span>
+						</dl>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/白黒.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/白黒.png">
+							</dt>
+							<dd class="BCDesc">表クロ/裏クロ</dd>
+							<span>¥000</span>
+						</dl>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/カラー.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/なし.png">
+								
+							</dt>
+							<dd class="BCDesc">表カラー/裏なし</dd>
+							<span>¥000</span>
+						</dl>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/カラー.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/白黒.png">
+								
+								</dt>
+							<dd class="BCDesc">表カラー/裏クロ</dd>
+							<span>¥000</span>
+						</dl>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/カラー.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/カラー.png">
+								
+							</dt>
+							<dd class="BCDesc">表カラー/裏カラー</dd>
+							<span>¥000</span>
+						</dl>
+
+						
+
+
+					</dd>
+				</dl>
+
+				<dl class="priceTableCell ClearFix">
+					<dt class="busuinessCardName">超格安名刺</dt>
+					<dd>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/白黒.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/なし.png">
+							</dt>
+							<dd class="BCDesc">表クロ/裏なし</dd>
+							<span>¥000</span>
+						</dl>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/白黒.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/白黒.png">
+							</dt>
+							<dd class="BCDesc">表クロ/裏クロ</dd>
+							<span>¥000</span>
+						</dl>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/カラー.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/なし.png">
+								
+							</dt>
+							<dd class="BCDesc">表カラー/裏なし</dd>
+							<span>¥000</span>
+						</dl>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/カラー.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/白黒.png">
+								
+								</dt>
+							<dd class="BCDesc">表カラー/裏クロ</dd>
+							<span>¥000</span>
+						</dl>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/カラー.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/カラー.png">
+								
+							</dt>
+							<dd class="BCDesc">表カラー/裏カラー</dd>
+							<span>¥000</span>
+						</dl>
+
+						
+
+
+					</dd>
+				</dl>
+
+				<dl class="priceTableCell ClearFix">
+					<dt class="busuinessCardName">超格安名刺</dt>
+					<dd>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/白黒.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/なし.png">
+							</dt>
+							<dd class="BCDesc">表クロ/裏なし</dd>
+							<span>¥000</span>
+						</dl>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/白黒.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/白黒.png">
+							</dt>
+							<dd class="BCDesc">表クロ/裏クロ</dd>
+							<span>¥000</span>
+						</dl>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/カラー.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/なし.png">
+								
+							</dt>
+							<dd class="BCDesc">表カラー/裏なし</dd>
+							<span>¥000</span>
+						</dl>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/カラー.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/白黒.png">
+								
+								</dt>
+							<dd class="BCDesc">表カラー/裏クロ</dd>
+							<span>¥000</span>
+						</dl>
+						<dl class="busuinessCardType">
+							<dt class="BCimage">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/カラー.png">
+								<img src="<?php bloginfo('template_url'); ?>/images/PriceTable/カラー.png">
+								
+							</dt>
+							<dd class="BCDesc">表カラー/裏カラー</dd>
+							<span>¥000</span>
+						</dl>
+
+						
+
+
+					</dd>
+				</dl>
+
+
+			</dd>
+		</dl>
+
+	</div><!--/.fieldWrap -->
