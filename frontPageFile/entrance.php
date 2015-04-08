2abd9d4b48c74d82ff638e530e25989f2d33c77e
kazuya <aribaba845@gmail.com>
2015-04-08 21:50:50 +0900

    test

--- frontPageFile/entrance.php
+++ frontPageFile/entrance.php
@@ -8,9 +8,9 @@
 			</div><!-- /siteLogoField -->
 			<span id="subDesc">オーダメイド名刺であなたの印象をデザインする</span>
 		</div><!-- /header -->
-	<div class="fieldWrap" >
+<!-- 	<div class="fieldWrap" >
 		<img id="topLogo" src="<?php bloginfo('template_url'); ?>/images/entrance/entranceLogo_119x100.png" alt="">
 		<img id="topMeiship" src="<?php bloginfo('template_url'); ?>/images/entrance/MEiSHiP_570x100.png" alt="">
 		<img id="desc" src="<?php bloginfo('template_url'); ?>/images/entrance/design_your_impression_587x50.png" alt="">
 	
-	</div><!--/.fieldWrap -->
+	</div><!--/.fieldWrap --> -->
