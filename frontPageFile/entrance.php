7cdb973bf3aecddb7fd52d0f506af4714de4f6db
kazuya <aribaba845@gmail.com>
2015-04-08 21:16:24 +0900

    エントランス 修正

--- frontPageFile/entrance.php
+++ frontPageFile/entrance.php
@@ -1,5 +1,13 @@
 	<!-- entrance -->
 
+		<div id="dammyHeader" class="ClearFix">
+
+			<div id="siteLogoField">
+				<img id="logo" src="<?php bloginfo('template_url'); ?>/images/Header/headerLogo71x60.png" alt="">
+					<img id="meiship" src="<?php bloginfo('template_url'); ?>/images/Header/headerLogoMeishipWhite.png" alt="">						
+			</div><!-- /siteLogoField -->
+			<span id="subDesc">オーダメイド名刺であなたの印象をデザインする</span>
+		</div><!-- /header -->
 	<div class="fieldWrap" >
 		<img id="topLogo" src="<?php bloginfo('template_url'); ?>/images/entrance/entranceLogo_119x100.png" alt="">
 		<img id="topMeiship" src="<?php bloginfo('template_url'); ?>/images/entrance/MEiSHiP_570x100.png" alt="">
